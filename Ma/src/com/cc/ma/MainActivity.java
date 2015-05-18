265304b56f10b0464c759790c74d5d63ced6459e
wangcheng <wangcheng3986@navinfo.com>
2015-05-18 16:29:41 +0800

    引导界面

---
+++ Ma/src/com/cc/ma/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cc.ma;
+
+import com.cc.ma.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
